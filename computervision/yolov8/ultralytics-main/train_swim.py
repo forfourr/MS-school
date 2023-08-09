cef5f259e36cc10167804aa22e6a3c2288911982
forfourr <iiilee0404@gmail.com>
2023-08-09 11:50:31 +0000

    yolo_swim data

---
+++ computervision/yolov8/ultralytics-main/train_swim.py
@@ -0,0 +1,6 @@
+from ultralytics import YOLO
+if __name__ == "__main__":
+    model = YOLO('yolov8s.pt')
+    model.train(data='yolov8/ultralytics-main/ultralytics/cfg/datasets/swim_yaml.yaml', epochs=20,batch=34, degrees=5, lrf=0.025)
+
+
